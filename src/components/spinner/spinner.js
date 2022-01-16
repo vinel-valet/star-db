21ac61590182fff8bc436c5f00e96379092813ff
vinel-valet <kolya100likvidator@yandex.ru>
2022-01-16 16:05:39 +0300

    add spinner

--- src/components/spinner/spinner.js
+++ src/components/spinner/spinner.js
@@ -6,7 +6,8 @@ const Spinner = () => {
     return (
         <div className="lds-css">
             <div className="lds-double-ring">
-
+                <div></div>
+                <div></div>
             </div>
         </div>
     );
