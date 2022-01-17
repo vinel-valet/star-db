801a30066340b89a1158a2767dde087478efddbf
Nikolay <94299978+vinel-valet@users.noreply.github.com>
2022-01-17 16:37:16 +0300

    list-item

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import Header from '../header/header';
 import RandomPlanet from '../random-planet/random-planet';
@@ -7,22 +7,33 @@ import PersonDetails from '../person-details/person-details';
 
 import './app.css';
 
-const App = () => {
+export default class App extends Component{
+
+  state = {
+    selectedPerson: null
+  }
+
+  onPersonSelected = (id) => {
+     this.setState({
+       selectedPerson: id
+     })
+  }
+
+  render() {
     return (
-        <div>
-            <Header />
-            <RandomPlanet />
+      <div>
+        <Header />
+        <RandomPlanet />
 
-            <div className="row mb2">
-                <div className="col-md-6">
-                    <ItemList />
-                </div>
-                <div className="col-md-6">
-                    <PersonDetails />
-                </div>
-            </div>
+        <div className="row mb2">
+          <div className="col-md-6">
+            <ItemList onItemSelected = {this.onPersonSelected}/>
+          </div>
+          <div className="col-md-6">
+            <PersonDetails personId = {this.state.selectedPerson}/>
+          </div>
         </div>
+      </div>
     );
+  }
 };
-
-export default App;
--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import './random-planet.css';
 import SwapiService from "../../services/swapi-service";
 import Spinner from "../spinner/spinner";
+import ErrorIndicator from "../error-indicator/error-indicator";
 
 export default class RandomPlanet extends Component {
 
@@ -11,35 +12,45 @@ export default class RandomPlanet extends Component {
 
     state = {
         planet: {},
-        loading: true
+        loading: true,
+        error: false
     }
 
-    constructor() {
-        super();
-        this.updatePlanet();
+    componentDidMount() {
+        this.updatePlanet()
     }
 
     onPlanetLoaded = (planet) => {
         this.setState({planet, loading: false})
     }
 
+    onError = (err) => {
+        this.setState({
+            error: true,
+            loading: false
+        })
+    }
+
     updatePlanet() {
-        const id = Math.floor(Math.random() * 25) + 2;
+        const id = Math.floor(Math.random() * 20) + 3;
         this.swapiService
             .getPlanet(id)
             .then(this.onPlanetLoaded)
+            .catch(this.onError)
     }
 
     render() {
-        const {planet, loading} = this.state
+        const {planet, loading, error} = this.state
 
+        const errorIndicator = error ? <ErrorIndicator/> : null;
         const spinner = loading ? <Spinner/> : null;
-        const content = !loading ? <PlanetView planet = {planet} /> : null;
+        const content = !(loading || error) ? <PlanetView planet = {planet} /> : null;
 
 
         return (
             <div className="random-planet jumbotron rounded">
                 {spinner}
+                {errorIndicator}
                 {content}
             </div>
 
